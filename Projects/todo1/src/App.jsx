7f5aea6db7b5d09e745f803ff78190ddcadcec1f
mahendharbillakuri <billakurimahendhar@gmail.com>
2025-04-13 14:02:49 +0530

    Bhara clock

--- Projects/todo1/src/App.jsx
+++ Projects/todo1/src/App.jsx
@@ -5,11 +5,16 @@ import Todoitem1 from "./components/Todoitem1"
 import Todoitem2 from "./components/Todoitem2"
 import './App.css';
 function App() {
-  return <center class='todo'>
+  return <center className='todo'>
     <Appname></Appname>
     <Addtodo></Addtodo>
+    <div className='items'>
+
     <Todoitem1></Todoitem1>
     <Todoitem2></Todoitem2>
+
+    </div>
+    
   </center>
 }
 
--- Projects/todo1/src/components/Todoitem2.jsx
+++ Projects/todo1/src/components/Todoitem2.jsx
@@ -1,14 +1,14 @@
 function Todoitem2(){
-  return  <div class="container text-center">
+  return  <div class="container">
 
-      <div class="row">
+      <div class="row kg-row">
         <div class="col-6">
           Eating
         </div>
         <div class="col-4">
           3/5/2003
         </div>
-        <div class="col-2"><button type="button" class="btn btn-danger">Delete</button></div>
+        <div class="col-2"><button type="button" class="btn btn-danger kg-button">Delete</button></div>
       </div>
     </div>
 }
